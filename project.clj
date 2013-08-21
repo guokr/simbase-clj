2e2b497e452136288ec5e07de320d8f4a3245abb
Mingli Yuan <mingli.yuan@gmail.com>
2013-08-21 11:50:50 +0800

    init project

---
+++ project.clj
@@ -0,0 +1,17 @@
+(defproject com.guokr/simbase-clj "0.0.0"
+
+    :description "A clojure client for simbase document similarity server"
+    :url "https://github.com/guokr/simbase-clj/"
+
+    :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "same as Clojure"}
+
+    :dependencies [[org.clojure/clojure "1.5.1"]
+                   [com.taoensso/carmine "2.0.0"]]
+    :source-paths ["src"]
+
+    :compile-path "target/classes"
+    :target-path "target/"
+    :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"])
